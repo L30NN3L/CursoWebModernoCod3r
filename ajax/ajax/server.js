6a2c44a82c8697f40cd98d6a0118983c33a6cc60
Hugo Leonel <hugoleone.net@gmail.com>
2018-07-21 19:23:52 -0300

    XMLHttpRequest #01

--- ajax/ajax/server.js
+++ ajax/ajax/server.js
@@ -2,9 +2,9 @@ const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
 
-app.use(express.static('.'))
+app.use(express.static('.')) // serve todos os arquivos estáticos
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
-app.get('/teste', (req, res) => res.send('Ok'))
+// app.get('/teste', (req, res) => res.send('Ok'))
 app.listen(8080, () => console.log('Executando...'))
