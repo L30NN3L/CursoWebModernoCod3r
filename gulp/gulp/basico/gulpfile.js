af8ac76b3013637f063d748337c7e071f4c8bc74
Hugo Leonel <hugoleone.net@gmail.com>
2018-07-22 23:31:58 -0300

    Alterações GULP

---
+++ gulp/gulp/basico/gulpfile.js
@@ -0,0 +1,34 @@
+const gulp = require('gulp')
+
+gulp.task('default', () => {
+    gulp.start('copiar', 'fim')
+})
+
+gulp.task('copiar', ['antes1', 'antes2'], () => {
+    
+    gulp.src(['pastaA/arquivo1.txt', 'pastaA/arquivo2.txt'])
+    // ou gulp.src('pastaA/*.txt')
+    // ou gulp.src('pastaA/**/*.txt')
+        // .pipe(transformacao1())
+        // .pipe(transformacao2())
+        .pipe(gulp.dest('pastaB'))
+    // console.log('Copiar!!!')
+})
+
+gulp.task('antes1', () => {
+    console.log('Antes 1!!!')
+})
+
+gulp.task('antes2', () => {
+    console.log('Antes 2!!!')
+})
+
+gulp.task('fim', ['fim1', 'fim2'])
+
+gulp.task('fim1', () => {
+    console.log('Fim 1!!!')
+} )
+
+gulp.task('fim2', () => {
+    console.log('Fim 2!!!')
+} )
